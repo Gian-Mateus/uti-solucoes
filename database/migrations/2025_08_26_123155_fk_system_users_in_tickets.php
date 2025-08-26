ada480ed5157567a353a819b88f3c51bc5d0a9c9
gian.m.silver <gian.m.silver@gmail.com>
2025-08-26 14:05:36 +0000

    refact: add fk system users in tickets

---
+++ database/migrations/2025_08_26_123155_fk_system_users_in_tickets.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->foreignId('responsibleUser')
+                    ->references('id')
+                    ->on('system_users')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('system_users', function (Blueprint $table) {
+            $table->dropForeign(['tickets_id']);
+            $table->dropColumn('tickets_id');
+        });
+    }
+};
