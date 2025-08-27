afdcac0d2fde852a32679b076ef5b684dc7b3664
gian.m.silver <gian.m.silver@gmail.com>
2025-08-27 20:57:20 +0000

    feat: criado tela de login para o sistema

--- resources/views/app/public/login.blade.php
+++ resources/views/app/public/login.blade.php
@@ -6,12 +6,12 @@
             separator
             class="text-primary"
         />
-        <x-form class="mt-10">
+        <x-form method="POST" class="mt-10" action="{{ route('system.auth') }}">
             @method('POST')
             @csrf
             <div class="px-10 grid grid-cols-1 gap-4">
-                <x-input label="Usuário" type="text" icon="o-user" placeholder="Usuário"/>
-                <x-input label="Senha" type="password" icon="o-key" placeholder="*******"/>
+                <x-input label="Usuário" type="text" icon="o-user" name="login" placeholder="Usuário"/>
+                <x-input label="Senha" type="password" icon="o-key" name="password" placeholder="*******"/>
                 <x-menu-separator />
                 <x-button type="submit" label="Entrar" class="btn btn-primary"/>
             </div>
