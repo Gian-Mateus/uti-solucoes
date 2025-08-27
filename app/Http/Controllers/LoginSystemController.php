96e23272a2f3c828228031f227896ec3a5d339d1
gian.m.silver <gian.m.silver@gmail.com>
2025-08-27 20:54:32 +0000

    feat: adicionando função de autenticação para o sistema

--- app/Http/Controllers/LoginSystemController.php
+++ app/Http/Controllers/LoginSystemController.php
@@ -3,10 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginSystemController extends Controller
 {
     public function index(){
+        if (Auth::guard('system')->check()) {
+            return redirect()->route('system.home');
+        }
         return view('app.public.login');
+    }
+
+    public function authenticate(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'login' => ['required'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::guard('system')->attempt($credentials)) {
+            $request->session()->regenerate();
+ 
+            return redirect()->intended(route('system.home'));
+        }
+ 
+        return back()->withErrors([
+            'login' => 'The provided credentials do not match our records.',
+        ])->onlyInput('login');
     }
 }
