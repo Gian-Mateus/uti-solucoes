f86d1859db63d3495016b90551130652ababff22
gian.m.silver <gian.m.silver@gmail.com>
2025-08-27 20:57:37 +0000

    feat: criando função de autenticação para clientes

--- app/Http/Controllers/LoginClientController.php
+++ app/Http/Controllers/LoginClientController.php
@@ -9,4 +9,22 @@ class LoginClientController extends Controller
     public function index(){
         return view('page.login');
     }
+
+    public function authenticate(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'login' => ['required'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+ 
+            return redirect()->intended('dashboard');
+        }
+ 
+        return back()->withErrors([
+            'login' => 'The provided credentials do not match our records.',
+        ])->onlyInput('login');
+    }
 }
