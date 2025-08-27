463c4738e7e35b5388c3bb1fac9b236761ede5a5
gian.m.silver <gian.m.silver@gmail.com>
2025-08-27 20:57:01 +0000

    refact: ajustando o template padrão do app

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -14,11 +14,74 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+@unless (Auth::check())
 <body class="bg-base flex items-center lg:justify-center min-h-screen flex-col font-sans">
     <header class="text-primary text-4xl font-bold p-8 flex gap-4 items-center">
         <img class="w-24 h-auto" src="/logo.png" alt="Logotipo UTI Soluções em Informática">
         <h1>Sistema</h1>
     </header>
-    {{ $slot }}
+    
+    <main class="flex-1 mx-auto">
+        {{ $slot }}
+    </main>
 </body>
+@endunless
+
+@if(Auth::check())
+<body class="min-h-screen font-sans antialiased bg-base-200">
+ 
+    {{-- NAVBAR mobile only --}}
+    <x-nav sticky class="lg:hidden">
+        <x-slot:brand>
+            <div class="ml-5 pt-5">App</div>
+        </x-slot:brand>
+        <x-slot:actions>
+            <label for="main-drawer" class="lg:hidden mr-3">
+                <x-icon name="o-bars-3" class="cursor-pointer" />
+            </label>
+        </x-slot:actions>
+    </x-nav>
+ 
+    {{-- MAIN --}}
+    <x-main full-width>
+        {{-- SIDEBAR --}}
+        <x-slot:sidebar drawer="main-drawer" collapsible class="bg-base-100 lg:bg-inherit">
+ 
+            {{-- BRAND --}}
+            <div class="ml-5 pt-5">App</div>
+ 
+            {{-- MENU --}}
+            <x-menu activate-by-route>
+ 
+                {{-- User --}}
+                @if($user = auth()->user())
+                    <x-menu-separator />
+ 
+                    <x-list-item :item="$user" value="name" sub-value="email" no-separator no-hover class="-mx-2 !-my-2 rounded">
+                        <x-slot:actions>
+                            <x-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logoff" no-wire-navigate link="/logout" />
+                        </x-slot:actions>
+                    </x-list-item>
+ 
+                    <x-menu-separator />
+                @endif
+ 
+                <x-menu-item title="Hello" icon="o-sparkles" link="/" />
+                <x-menu-sub title="Settings" icon="o-cog-6-tooth">
+                    <x-menu-item title="Wifi" icon="o-wifi" link="####" />
+                    <x-menu-item title="Archives" icon="o-archive-box" link="####" />
+                </x-menu-sub>
+            </x-menu>
+        </x-slot:sidebar>
+ 
+        {{-- The `$slot` goes here --}}
+        <x-slot:content>
+            {{ $slot }}
+        </x-slot:content>
+    </x-main>
+ 
+    {{-- Toast --}}
+    <x-toast />
+</body>
+@endif
 </html>
